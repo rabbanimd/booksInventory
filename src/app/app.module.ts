6b331575a24bfc2b8680edc6bc67420a70e09cb6
MD Rabbani <dev.rabbani>
2022-10-05 23:59:29 +0530

    infinite scrolling homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { HttpClientModule} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    InfiniteScrollModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,28 +11,59 @@ export class HomeComponent implements OnInit {
 
   constructor(private content:ContentService) { }
 
+  maxScrollLength = 10;
+
   booksPage ?: BookResponsePaged;
-  // pageNo = 0;
-  // pageSize = 10;
-  // totalPages = 1;
-  // isLast = false;
+  nOfResult = 0;
+  pageSize = 10;
+  totalPages = 1;
+  isLast = false;
   result :any;
+  pageNo = 0;
+  booksList : Array<BookResponse> = [];
 
   ngOnInit(): void {
-    this.content.fetchAllBooks().subscribe((data:BookResponsePaged)=> {
-      this.booksPage = {
-        pageNo:data.pageNo,
-        pageSize:data.pageSize,
-        totalPages:data.totalPages,
-        isLast : data.isLast,
-        books:data.books
-      }
-      console.log(this.booksPage);
-      // data.books?.forEach((book)=>this.booksPage?.books?.push(new BookResponse(book.id, book.bookName, book.authorName, book.pages, book.isbn, book.description)))
-    },err=>{
-      console.log("ERROR : "+ err);
-    });
-    // console.log("page no : "+this.booksPage?.pageNo);
+    this.getBooksPage();
+  //     console.log(this.booksPage);
+  //     // data.books?.forEach((book)=>this.booksPage?.books?.push(new BookResponse(book.id, book.bookName, book.authorName, book.pages, book.isbn, book.description)))
+  //   },err=>{
+  //     console.log("ERROR : "+ err);
+  //   });
+  //   // console.log("page no : "+this.booksPage?.pageNo);
   }
 
+  onScroll(ev:any) {
+    if(this.isLast || this.maxScrollLength === 2){
+      return;
+    }
+    this.maxScrollLength++;
+    this.pageNo++;
+    console.log("scrolled : "+ev+"page : "+this.pageNo);
+
+    this.getBooksPage();
+  }
+
+  getBooksPage() {
+    this.content.fetchAllBooksPage(this.pageNo, this.pageSize).subscribe(
+      (data:BookResponsePaged)=> {
+        this.booksPage = {
+          pageNo:data.pageNo,
+          pageSize:data.pageSize,
+          totalPages:data.totalPages,
+          isLast : data.isLast,
+          books:data.books
+        };
+        this.totalPages = this.booksPage.totalPages;
+        this.isLast = this.booksPage.isLast;
+        this.booksPage.books.forEach((book:BookResponse)=>this.booksList.push(book));
+        this.nOfResult += this.booksPage.pageSize;
+      },
+        err=>{
+          console.log("ERROR : "+ err);
+        }
+      );
+  }
+
+
+  
 }
